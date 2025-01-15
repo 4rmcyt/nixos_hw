8d7f0371a6a1c3c661a3eedc2e80f643231a1ed7
4rmcyt <4rmcyt@gmail.com>
2025-01-15 00:53:33 -0700

    Refactor NixOS configuration for improved readability and structure

--- nixos/nixosConfigurations/nixos.nix
+++ nixos/nixosConfigurations/nixos.nix
@@ -1,4 +1,4 @@
-{ inputs, ... }@flakeContext:
+{ inputs, ... } @flakeContext:
 let
   nixosModule = { config, lib, pkgs, ... }: {
     config = {
@@ -9,90 +9,78 @@ let
           };
         };
       };
-      fileSystems = fileSystems {
-        boot.loader.systemd-boot.enable = true;
-
-        fileSystems."/boot" = {
-          device = "" /dev/disk/by-partlabel/ESP ";
-            		fsType = "
-            fat32 ";
-            	};
-
-            	fileSystems."/" = {
-		device = " /dev/disk/by-partlabel/root ";
-            		fsType = "
-            ext4 ";
-            	};
-              networking = {
-                enableIPv6 = true;
-                firewall = {
-                  allowedTCPPorts = [
-                    80
-                    22
-                  ];
-                  enable = true;
-                };
-                useDHCP = true;
-              };
-              security = {
-                sudo = {
-                  wheelNeedsPassword = true;
-                };
-              };
-              services = {
-                fail2ban = {
-                  enable = true;
-                  maxretry = 3;
-                };
-                nginx = {
-                  enable = true;
-                  virtualHosts = {
-                    nixos = {
-                      default = true;
-                      root = ((
-            builtins.fetchTarball {
-            url=" https://storage.mynixos.com/1499/resources/36039c27-7f93-4a38-ac38-fa4f01717caa/3ef8cd170e51cb28478a49013f62f3b4.jpg.tar.gz ";
-            sha256=" 8
-            lwb9lZSPGLACg9IJtaRGqmcoCffiZ3GxM9MHN0SolY=";
-}
-) + "/3ef8cd170e51cb28478a49013f62f3b4.jpg");
+      fileSystems = {
+        "/boot" = {
+          device = "/dev/disk/by-partlabel/ESP";
+          fsType = "fat32";
+        };
+        "/" = {
+          device = "/dev/disk/by-partlabel/root";
+          fsType = "ext4";
+        };
+      };
+      networking = {
+        enableIPv6 = true;
+        firewall = {
+          allowedTCPPorts = [
+            80
+            22
+          ];
+          enable = true;
+        };
+        useDHCP = true;
+      };
+      security = {
+        sudo = {
+          wheelNeedsPassword = true;
+        };
+      };
+      services = {
+        fail2ban = {
+          enable = true;
+          maxretry = 3;
+        };
+        nginx = {
+          enable = true;
+          virtualHosts = {
+            nixos = {
+              default = true;
+              root = builtins.fetchTarball {
+                url = "https://storage.mynixos.com/1499/resources/36039c27-7f93-4a38-ac38-fa4f01717caa/3ef8cd170e51cb28478a49013f62f3b4.jpg.tar.gz";
+                sha256 = "8lwb9lZSPGLACg9IJtaRGqmcoCffiZ3GxM9MHN0SolY=";
+              } + "/3ef8cd170e51cb28478a49013f62f3b4.jpg";
+            };
+          };
+        };
+        openssh = {
+          enable = true;
+          settings = {
+            AllowUsers = [ "nixos" ];
+          };
+        };
+      };
+      system = {
+        autoUpgrade = {
+          enable = true;
+        };
+      };
+      users = {
+        extraGroups = {
+          nixos = {
+            name = "wheel systemd-journal";
+          };
+        };
+        users = {
+          nixos = {
+            hashedPassword = "$y$j9T$P1v6RUEKYIfi04PQdHN1r.$.jeqXdBx5L32T60NGZyvU1Q/.sXmDyO/ZOMPVEb3AB/";
+            isNormalUser = true;
+          };
         };
       };
     };
-    openssh = {
-      enable = true;
-      settings = {
-        AllowUsers = [
-          "nixos"
-        ];
-      };
-    };
   };
-  system = {
-    autoUpgrade = {
-      enable = true;
-    };
-  };
-  users = {
-    extraGroups = {
-      nixos = {
-        name = wheel systemd-journal;
-      };
-    };
-    users = {
-      nixos = {
-        hashedPassword = $y$j9T$P1v6RUEKYIfi04PQdHN1r.$.jeqXdBx5L32T60NGZyvU1Q/.sXmDyO/ZOMPVEb3AB/;
-        isNormalUser = true;
-      };
-    };
-  };
-};
-};
 in
 inputs.nixpkgs.lib.nixosSystem {
-modules=[
-  nixosModule
-];
-system="x86_64-linux" ;
+  modules = [ nixosModule ];
+  system = "x86_64-linux";
 }
-
